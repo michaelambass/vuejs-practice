4087f9727b0edaea54ab283383c3883fa0af1a93
Michael Ambass <michael.ambass@gmail.com>
2016-12-05 17:45:35 +0100

    Methods interaction

--- js/app.js
+++ js/app.js
@@ -10,6 +10,19 @@ new Vue({
     link: 'http://www.google.com',
     success: false,
     persons: ['John', 'Marion', 'Marine', 'Jean', 'Patrick']
+  },
+
+  // actions
+  methods: {
+    close: function () {
+
+      if(this.success == true){
+        this.success = false;
+      }else{
+        this.success = true;
+      }
+      
+    }
   }
 
 
