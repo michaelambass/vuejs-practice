0563d8cfd3a05148ee616cbb053bc76f6d41b26b
Michael Ambass <michael.ambass@gmail.com>
2016-12-06 10:14:30 +0100

    Instance first steps

---
+++ 02-instance/js/app.js
@@ -0,0 +1,40 @@
+// VueJS instanciation
+var app = new Vue({
+
+  // selector
+  el: '#app',
+
+  // data - contains all variables
+  // if inspected, the object will have for each data a get & set informations
+  // note : all the datas must be declared here before (no dynamic for the moment (see assign))
+  data: {
+    message: 'Salut les gens',
+    link: 'http://www.google.com',
+    success: false,
+    persons: ['John', 'Marion', 'Marine', 'Jean', 'Patrick']
+  },
+
+  // actions
+  methods: {
+    close: function () {
+
+      // Toggle success variable boolean
+      if(this.success == true){
+        this.success = false;
+      }else{
+        this.success = true;
+      }
+
+    },
+
+    addPerson: function() {
+
+      // Add Marion
+      this.persons.push('Marion');
+
+    }
+
+  }
+
+
+})
